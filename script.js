d639508df272efaf1fa6915f654539f6e327441e
abijith <abijitka02@gmail.com>
2022-01-23 13:38:09 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function buttonclick(val){
+    
+    
+    document.getElementById("screen").value+=val;
+}
+function clearme()
+{
+    document.getElementById("screen").value=""
+}
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
